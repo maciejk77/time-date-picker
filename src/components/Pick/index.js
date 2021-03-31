38e67d0527a5fe136779f3ec570a27107ff15d92
Maciej Kurek <maciejk77@gmail.com>
2021-03-31 10:26:17 +0100

    DRY selected day/time flag functions into helper

--- src/components/Pick/index.js
+++ src/components/Pick/index.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { isActiveSlot } from '../../helpers';
-import { DAY } from '../../constants';
+import { DAY, TIME } from '../../constants';
 import { Store } from '../../Store';
 import StyledPick from './styles';
 
@@ -14,13 +14,19 @@ const Pick = ({ data, value, onClick, type, customStyles, children }) => {
   const disabled = type !== DAY && !isActiveSlot(value);
 
   // below could be DRY-ied
-  const selectedTime = state.selectedTime
-    ? value === state.selectedTime.time
-    : false;
+  // const selectedTime = state.selectedTime
+  //   ? value === state.selectedTime.time
+  //   : false;
 
-  const selectedDay = state.selectedDay
-    ? value === state.selectedDay.day
-    : false;
+  // const selectedDay = state.selectedDay
+  //   ? value === state.selectedDay.day
+  //   : false;
+
+  const selectedItem = (stateItem, key) =>
+    stateItem ? value === stateItem[key] : false;
+
+  const selectedDay = selectedItem(state.selectedDay, DAY);
+  const selectedTime = selectedItem(state.selectedTime, TIME);
 
   const selected = selectedDay || selectedTime;
 
